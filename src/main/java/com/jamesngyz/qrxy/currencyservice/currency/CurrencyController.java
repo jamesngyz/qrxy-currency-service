a3c69a4045ce746bf4d7ba327fe26be8edeb2e85
James Ng <hello@jamesngyz.com>
2020-01-26 14:38:12 +0800

    Add POST currencies response body

--- src/main/java/com/jamesngyz/qrxy/currencyservice/currency/CurrencyController.java
+++ src/main/java/com/jamesngyz/qrxy/currencyservice/currency/CurrencyController.java
@@ -2,15 +2,25 @@ package com.jamesngyz.qrxy.currencyservice.currency;
 
 import java.net.URI;
 
+import org.mapstruct.factory.Mappers;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyController {
 	
-	@PostMapping(path = "/v1/currencies")
-	public ResponseEntity createCurrency() {
-		return ResponseEntity.created(URI.create("")).build();
+	private final CurrencyDtoMapper currencyDtoMapper = Mappers.getMapper(CurrencyDtoMapper.class);
+	
+	@PostMapping(path = "/v1/currencies", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<CurrencyResponse> createCurrency(@RequestBody CurrencyRequest request) {
+		
+		Currency currency = currencyDtoMapper.requestToCurrency(request);
+		CurrencyResponse response = currencyDtoMapper.currencyToResponse(currency);
+		
+		return ResponseEntity.created(URI.create("")).body(response);
 	}
+	
 }
