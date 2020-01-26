3033ae366cc7c10d7585925f7593259f83363dfe
James Ng <hello@jamesngyz.com>
2020-01-27 01:51:14 +0800

    Add CurrencyRepository
    Test CurrencyService createCurrency return value
    Update CurrencyService createCurrency return value
    Update expected CurrencyService createCurrency return value
    Add CurrencyRepository to CurrencyService; Update createCurrency
    
    Add H2DB
    Add Repository annotation to CurrencyRepository
    Add Entity, Id annotations to Currency
    Call CurrencyRepository#save from CurrencyService#createCurrency

--- src/main/java/com/jamesngyz/qrxy/currencyservice/currency/CurrencyService.java
+++ src/main/java/com/jamesngyz/qrxy/currencyservice/currency/CurrencyService.java
@@ -1,13 +1,20 @@
 package com.jamesngyz.qrxy.currencyservice.currency;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurrencyService {
 	
+	private final CurrencyRepository repository;
+	
+	@Autowired
+	public CurrencyService(CurrencyRepository repository) {
+		this.repository = repository;
+	}
+	
 	public Currency createCurrency(Currency currency) {
-		// TODO
-		return null;
+		return repository.save(currency);
 	}
 	
 }
