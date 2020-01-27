74f43a5c7f433b25dd0156d2390e2a06100ff98d
James Ng <hello@jamesngyz.com>
2020-01-27 15:29:02 +0800

    Add CurrencyRepository to CurrencyService; Update createCurrency
    Add H2DB
    Add Repository annotation to CurrencyRepository
    Add Entity, Id annotations to Currency
    Call CurrencyRepository#save from CurrencyService#createCurrency

--- src/main/java/com/jamesngyz/qrxy/currencyservice/currency/Currency.java
+++ src/main/java/com/jamesngyz/qrxy/currencyservice/currency/Currency.java
@@ -3,11 +3,16 @@ package com.jamesngyz.qrxy.currencyservice.currency;
 import java.util.Date;
 import java.util.UUID;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Data
+@Entity
 public class Currency {
 	
+	@Id
 	private UUID id;
 	
 	private String code;
--- src/main/java/com/jamesngyz/qrxy/currencyservice/currency/CurrencyRepository.java
+++ src/main/java/com/jamesngyz/qrxy/currencyservice/currency/CurrencyRepository.java
@@ -3,6 +3,8 @@ package com.jamesngyz.qrxy.currencyservice.currency;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CurrencyRepository extends JpaRepository<Currency, UUID> {
 }
