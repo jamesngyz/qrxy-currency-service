0d39d367eedc2e373b86f640b6daab24b6d91e7b
James Ng <hello@jamesngyz.com>
2020-03-23 00:44:40 +0800

    Add postgresql; Add app integration test;  Add config per profile

---
+++ src/it/java/com/jamesngyz/qrxy/currencyservice/ApplicationIT.java
@@ -0,0 +1,15 @@
+package com.jamesngyz.qrxy.currencyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("integration-test")
+class ApplicationIT {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
