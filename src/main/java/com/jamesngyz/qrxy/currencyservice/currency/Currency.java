ed572a9b6df133b15576be1ef9610b84b190e590
James Ng <hello@jamesngyz.com>
2020-03-30 00:58:18 +0800

    Add table annotations to Currency

--- src/main/java/com/jamesngyz/qrxy/currencyservice/currency/Currency.java
+++ src/main/java/com/jamesngyz/qrxy/currencyservice/currency/Currency.java
@@ -13,33 +13,43 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.Data;
 
-@Data
 @Entity
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "currency")
+@Data
 public class Currency {
 	
+	@Column(name = "id")
 	@Id
 	private UUID id;
 	
+	@Column(name = "code")
 	private String code;
 	
+	@Column(name = "name")
 	private String name;
 	
+	@Column(name = "status")
 	@Enumerated(EnumType.STRING)
 	private Status status;
 	
+	@Column(name = "created_at")
 	@CreatedDate
 	private Date createdAt;
 	
+	@Column(name = "created_by")
 	@CreatedBy
 	private String createdBy;
 	
+	@Column(name = "updated_at")
 	@LastModifiedDate
 	private Date updatedAt;
 	
+	@Column(name = "updated_by")
 	@LastModifiedBy
 	private String updatedBy;
 	
+	@Column(name = "version")
 	@Version
 	private Integer version;
 	
